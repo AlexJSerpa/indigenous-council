17d9919aa0b233ab9cf72135968c2c927ddaebab
Alex Serpa <alex.serpa@jala.university>
2024-04-24 11:36:43 -0500

    Increase board panel

--- src/main/java/org/example/cabildomanager/view/HelloApplication.java
+++ src/main/java/org/example/cabildomanager/view/HelloApplication.java
@@ -10,8 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.
+                getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1200, 720);
         stage.setTitle("Cabildo!");
         stage.setScene(scene);
         stage.show();
